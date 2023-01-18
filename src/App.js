f88c6e9865afb5f931bf3052aa438168eba6e563
Steven Bryant <stevelad_83@yahoo.com>
2023-01-18 18:08:28 -0500

    fetched country data from supabase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
+import { fetchCountries } from './services/countries.js';
 
 function App() {
-  return <div className="App"></div>;
+  fetchCountries();
+  return <div className="App">;</div>;
 }
 
 export default App;
