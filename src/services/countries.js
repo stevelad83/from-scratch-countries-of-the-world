d0f9991cdfb6f947e2ec24c1d5dfb955231ae1f2
Steven Bryant <stevelad_83@yahoo.com>
2023-01-18 21:11:16 -0500

    removed console logs

--- src/services/countries.js
+++ src/services/countries.js
@@ -2,6 +2,5 @@ import { client, checkError } from './client.js';
 
 export async function fetchCountries() {
   const response = await client.from('countries').select('*');
-  console.log('response', response.data);
   return checkError(response);
 }
