107eac3365e20d929c3c8e5271b34de7d16e5461
Steven Bryant <stevelad_83@yahoo.com>
2023-01-18 20:37:26 -0500

    filter on select working

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useCountries from '../hooks/useCountries.js';
 import CountryCard from './CountryCard.js';
 
 export default function Main() {
   const countries = useCountries();
+  const [continent, setContinent] = useState('all');
+
+  const continents = [...new Set(countries.map(({ continent }) => continent))];
+
+  const filtered = countries.filter(
+    (country) => country.continent === continent || continent === 'all'
+  );
+
   return (
     <main>
-      {countries.map((country) => (
+      <h1>Flags of the World</h1>
+      <select onChange={(e) => setContinent(e.target.value)}>
+        <option value="all">All</option>
+        {continents.map((continent) => (
+          <option key={continent} value={continent}>
+            {continent}
+          </option>
+        ))}
+      </select>
+
+      {filtered.map((country) => (
         <CountryCard key={country.id} {...country} />
       ))}
     </main>
